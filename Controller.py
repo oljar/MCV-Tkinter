2dbe07248cb29e855f51b5a54ecca5814fe726fd
Jarosław Olszewski <oljar@tlen.pl>
2023-05-07 19:39:38 +0200

    first commit

---
+++ Controller.py
@@ -0,0 +1,36 @@
+import re
+import tkinter as tk
+from tkinter import ttk
+import source
+
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def save(self, email):
+        """
+        Save the email
+        :param email:
+        :return:
+        """
+        try:
+
+            # save the model
+            self.model.email = email
+            self.model.save()
+            print(self.model.email)
+            print(self.view.email_var1.get())
+            print(source.zrodlo)
+
+
+            # show a success message
+            self.view.show_success(f'The email {email} saved!')
+
+        except ValueError as error:
+            # show an error message
+            self.view.show_error(error)
+
+
+
+
